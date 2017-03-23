cd57e8eaf24bf56d45a1786e8431e4aab766bf6e
Andres Guachisaca <caguachisaca@gmail.com>
2017-03-23 15:43:19 -0500

    cambios vista ciudades

--- src/CooperativaDetailView.js
+++ src/CooperativaDetailView.js
@@ -7,10 +7,24 @@ import {
 
 export default class CooperativaDetailView extends Component {
 
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'Machala', 'Guayaquil', 'Santo Domingo', 'Duran', 'Cuenca', 'Loja', 'Quito', 'Ambato'
+      ])
+    };
+  }
+
   render () {
     return (
       <View>
         <Text>CooperativaDetailView</Text>
+        <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) => <Text>{rowData}</Text>}
+          />
       </View>
     )
   }
