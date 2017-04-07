3e3229cd6ac66e63f6289609d7c82da948f9a532
Adrian Conza <hola@adrianconza.com>
2017-04-06 19:47:35 -0500

    cambios tabbar

--- src/app.js
+++ src/app.js
@@ -1,31 +1,93 @@
 import React, { Component } from 'react'
-import { AppRegistry, Platform } from 'react-native'
+import { AppRegistry, Platform, Text, StyleSheet } from 'react-native'
 
 import {Scene, Router} from 'react-native-router-flux'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import LoginView from './views/LoginView'
-import PerfilView from './views/PerfilView'
 import SearchView from './views/SearchView'
 import OriginView from './views/OriginView'
 import DestinationView from './views/DestinationView'
-import DashboardView from './views/DashboardView'
 import TurnView from './views/TurnView'
+import InvoiceView from './views/InvoiceView'
+import PaymentView from './views/PaymentView'
+import PerfilView from './views/PerfilView'
+import DashboardView from './views/DashboardView'
 
 export default class OroTicketMobile extends Component {
   render () {
     const isAndroid = Platform.OS === 'android'
-    return <Router>
+    return <Router getSceneStyle={getSceneStyle}>
       <Scene key='root'>
         <Scene key='login' component={LoginView} hideNavBar />
         <Scene key='dashboard' component={DashboardView} hideNavBar />
         <Scene key='search' component={SearchView} hideNavBar />
-        <Scene key='origin' component={OriginView} title='Salidas' hideNavBar={isAndroid} />
-        <Scene key='destination' component={DestinationView} title='Destinos' hideNavBar={isAndroid} />
+        <Scene key='origin' component={OriginView} title='Salidas' hideNavBar={isAndroid} direction='vertical' />
+        <Scene key='destination' component={DestinationView} title='Destinos' hideNavBar={isAndroid} direction='vertical' />
         <Scene key='perfil' component={PerfilView} hideNavBar />
         <Scene key='turno' component={TurnView} hideNavBar />
       </Scene>
     </Router>
+    // return <Router getSceneStyle={getSceneStyle}>
+    //   <Scene key='root'>
+    //     <Scene key='login' component={LoginView} hideNavBar />
+    //     <Scene key='tabbar' tabs tabBarStyle={styles.tabBarStyle}
+    //       tabBarSelectedItemStyle={styles.tabBarSelectedItemStyle}>
+    //       <Scene key='searchRoot'
+    //         icon={() => <Icon name='search' size={32} color='#e74c3c' />}
+    //         navigationBarStyle={{backgroundColor: 'red'}}
+    //         titleStyle={{color: 'white'}}>
+    //         <Scene key='search' component={SearchView} hideNavBar />
+    //         <Scene key='origin' component={OriginView} title='Salidas' hideNavBar direction='vertical' />
+    //         <Scene key='destination' component={DestinationView} title='Destinos' hideNavBar={false} />
+    //       </Scene>
+    //       <Scene key='invoiceRoot'
+    //         icon={() => <Icon name='tags' size={32} color='#e74c3c' />}
+    //         navigationBarStyle={{backgroundColor: 'red'}}
+    //         titleStyle={{color: 'white'}}>
+    //         <Scene key='invoice' component={InvoiceView} hideNavBar />
+    //       </Scene>
+    //       <Scene key='paymentRoot'
+    //         icon={() => <Icon name='credit-card' size={32} color='#e74c3c' />}
+    //         navigationBarStyle={{backgroundColor: 'red'}}
+    //         titleStyle={{color: 'white'}}>
+    //         <Scene key='payment' component={PaymentView} hideNavBar />
+    //       </Scene>
+    //       <Scene key='perfilRoot'
+    //         icon={() => <Icon name='user-o' size={32} color='#e74c3c' />}
+    //         navigationBarStyle={{backgroundColor: 'red'}}
+    //         titleStyle={{color: 'white'}}>
+    //         <Scene key='perfil' component={PerfilView} hideNavBar />
+    //       </Scene>
+    //     </Scene>
+    //   </Scene>
+    // </Router>
   }
+}
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    backgroundColor: '#eee'
+  },
+  tabBarSelectedItemStyle: {
+    color: '#e74c3c'
+  }
+})
+
+const getSceneStyle = (/* NavigationSceneRendererProps */ props, computedProps) => {
+  const style = {
+    flex: 1,
+    backgroundColor: '#fff',
+    shadowColor: null,
+    shadowOffset: null,
+    shadowOpacity: null,
+    shadowRadius: null
+  }
+  if (computedProps.isActive) {
+    style.marginTop = computedProps.hideNavBar ? 0 : 50
+    style.marginBottom = computedProps.hideTabBar ? 0 : 0
+  }
+  return style
 }
 
 AppRegistry.registerComponent('OroTicketMobile', () => OroTicketMobile)
