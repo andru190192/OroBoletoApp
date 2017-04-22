802208c3ed47172e09f670d4f76c9593af2fb2f5
Carlos Ajila <carlos.ajila.m@gmail.com>
2017-04-22 11:27:58 -0500

    parameters

--- src/parameters.js
+++ src/parameters.js
@@ -1,12 +1,8 @@
 module.exports = {
   USER: '',
-<<<<<<< HEAD
-  // URL: 'https://oro-ticket-server.herokuapp.com/api',
-  URL: 'http://192.168.1.10:3000/api',
-=======
   URL: 'https://oro-ticket-server.herokuapp.com/api',
   // URL: 'http://192.168.1.10:3000/api',
->>>>>>> origin/master
+
   TOKEN: '',
   getHeader: function () {
     return new Headers({
